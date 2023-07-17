d748330d9fa1398464dc0ce6b4052fab4368071f
RicardoCH <rchambillap@unsa.edu.pe>
2023-07-17 16:12:43 -0500

    agregamos una simple casilla para que el usuario ingrese su ecuación

--- main/java/smartle_grafics/SmarTle.java
+++ main/java/smartle_grafics/SmarTle.java
@@ -1,20 +1,14 @@
 package SmarTle_Graphics.main.java.smartle_grafics;
 import SmarTle_Graphics.main.java.galapagos.*;
+import java.util.*;
+
+import javax.swing.JOptionPane;
 
 public class SmarTle {
 
   public static void main(String[] args) {
-    TurtleDrawingWindow draw = new TurtleDrawingWindow();
-    Turtle wally = new Turtle(Turtle.NO_DEFAULT_WINDOW);
-    wally.speed(5000);//al final tendremos que hacer que el origan siga a la tortuga
-    /* para que de smart explanatiosn, 10000 de velocidad es muy overkill */
-    /*
-     * es solo para no demorar en probar que grafique xd
-     si me lo imaginaba... entonces invocamos a los demás que ni los conozco
-     */
-    draw.add(wally);
-    draw.setUnit(0.5);
-    draw.setVisible(true);
+    Scanner sc = new Scanner(System.in);
+    
     //draw.setGrid(false);
     //Metodo para extraer coeficiente : yes
     //Metodo para extraer grado : no
@@ -31,9 +25,22 @@ public class SmarTle {
     //llamada de los terminos anidados recursivamente
     //exacto
     //no funciona ln
-    Equations conv = new Equations("x^(2) + x- 1", false);
-    System.out.println(conv.getTerms());
+    String laEcu = JOptionPane.showInputDialog(null, "Ingrese la función");
+    if(laEcu.length()== 0) return; 
+    Equations conv = new Equations(laEcu, false);
+    //System.out.println(conv.getTerms());
     //System.out.println(conv.rango());
+    TurtleDrawingWindow draw = new TurtleDrawingWindow();
+    Turtle wally = new Turtle(Turtle.NO_DEFAULT_WINDOW);
+    wally.speed(5000);//al final tendremos que hacer que el origan siga a la tortuga
+    /* para que de smart explanatiosn, 10000 de velocidad es muy overkill */
+    /*
+     * es solo para no demorar en probar que grafique xd
+     si me lo imaginaba... entonces invocamos a los demás que ni los conozco
+     */
+    draw.add(wally);
+    draw.setUnit(0.5);
+    draw.setVisible(true);
     DrawFunctions.draw(wally, conv);
   }
 } 
